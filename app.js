f8e6363482d2ede01a76c61a81b1e5fa06471908
Mursaleen <57497716+Syed-ul-Mursaleen@users.noreply.github.com>
2020-06-26 20:25:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function Get_num(num) {
+
+    var result = document.getElementById("input");
+    result.value += num;
+}
+
+function Clear_data() {
+
+    var result = document.getElementById("input")
+    result.value = "";
+}
+
+function Get_result() {
+
+    var result = document.getElementById("input");
+    result.value = eval(result.value);
+}
